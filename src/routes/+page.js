dd946742e25b7213a77c97848b7716bb0c38b4c8
marydotdev <mary@Marys-Mac-mini.local>
2023-04-19 09:41:07 -0400

    revert

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -7,25 +7,40 @@ export async function load({ fetch }) {
     //console.log(token)
 
     const res = await fetch(now_playing_endpoint, {
-		headers: {
-			Authorization: `Bearer ${token}`
-		}
-	})
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
 
-	if (res.status === 204 || res.status > 400) {
-		return {body: { isPlaying: false }}
-	}
+      if (res.status === 204 || res.status > 400) {
+        return {body: { isPlaying: false }}
+      }
 
-	const song = await res.json();
-	const isPlaying = song.is_playing;
-	const title = song.item.name;
-	const artist = song.item.artists.map((/** @type {{ name: any; }} */ _artist) => _artist.name).join(', ');
-	const album = song.item.album.name;
-	const albumImageUrl = song.item.album.images[0].url;
-	const songUrl = song.item.external_urls.spotify;
+      const data = await res.json();
+      const isPlaying = data.is_playing;
+      const trackType = data.currently_playing_type;
+      const song = data.item;
+      const episode = data.item;
+      const title = song?.name;
+      const artist = song?.artists.map((/** @type {{ name: any; }} */ _artist) => _artist.name).join(", ");
+      const album = song?.album.name;
+      const albumImageUrl = song?.album.images[0].url;
+      const songUrl = song?.external_urls.spotify;
 
-	return {
-	body: {title, artist, album, isPlaying, albumImageUrl, songUrl},
-	}
+      if (!isPlaying || trackType === 'undefined') {
+        return {body: { isPlaying: false }}
+      }
+
+      if (trackType === 'track') {
+        return {body: { isPlaying, song, title, artist, album, albumImageUrl, songUrl }}
+      }
+
+      if (trackType === 'episode') {
+        return {body: { isPlaying, episode }}
+      }
+
+      return {
+        body: { isPlaying, trackType },
+      }
   }
 
