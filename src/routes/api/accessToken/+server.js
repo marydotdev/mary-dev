a68eb6d441e1dcb6261541930b13b3a5467e5b74
Mary Haedrich <maryhaedrich@Lillies-MacBook-Air.local>
2023-04-21 15:23:05 -0400

    okay wow i feel stupid that worked

--- src/routes/api/accessToken/+server.js
+++ src/routes/api/accessToken/+server.js
@@ -5,10 +5,6 @@ import { SPOTIFY_REFRESH_TOKEN } from '$env/static/private';
 const client_id = SPOTIFY_CLIENT_ID;
 const client_secret = SPOTIFY_CLIENT_SECRET;
 const refresh_token = SPOTIFY_REFRESH_TOKEN;
-
-// const client_id = '718de912f7cc4c56adcc4390e4947ae7';
-// const client_secret = 'bd03e81580034cd29b78c07426c6dac2';
-// const refresh_token = 'AQC2McBQYhX1egJUHYGKERqB-6258QpMzaGFY5a-VgkUzCGEsf_Oe-jpO06Ofu6sKaACiVh1GT0W3YeRob806c3bFbvefG2dfr9ejcuRYOPjdclC9dC8jiPeOoY-ANFYAzE';
 const redirect_uri = "http://localhost:3000/"
 const token_endpoint = `https://accounts.spotify.com/api/token`;
 
