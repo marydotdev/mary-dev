69a1337b1988a574ea138168a5811868a0638c3b
marydotdev <mary.haedrich@gmail.com>
2023-09-26 11:46:44 -0400

    add hidden

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,8 @@
 import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
-  const { data } = await supabase.from("thoughts").select().order('id', { ascending: false }).limit(6);
+  const { data } = await supabase.from("thoughts").select().neq('hidden', true).order('id', { ascending: false }).limit(6);
+
   return {
     thoughts: data ?? [],
   };
